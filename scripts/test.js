d693e90b0659ef764cc1b3c7788f87785b01db30
steph-harris <steph.harris21@gmail.com>
2016-01-20 16:45:03 -0500

    changed collection to collapsible accordion, appended test ajax call to DOM

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   $('.parallax').parallax();
 
-  //for each item in collection, run ajax request to return city weather info
+  //for each list item in collapsible, run ajax request to return city weather info and append to that c-body
   $.ajax({
     type: "GET",
     url: "http://api.wunderground.com/api/d736e7063cf1620b/conditions/forecast/q/08873.json",
@@ -14,7 +14,9 @@ $(document).ready(function(){
       console.log(weather);
 
       for(i=0; i<weather.length; i++){
-        console.log("Conditions for " + weather[i].date.pretty + ": " + weather[i].conditions + " with a high of " + weather[i].high.fahrenheit + "f, and a low of " + weather[i].low.fahrenheit + "f");
+        var newConditions = ("<h5>Conditions for " + weather[i].date.pretty + ": " + weather[i].conditions + " with a high of " + weather[i].high.fahrenheit + "f, and a low of " + weather[i].low.fahrenheit + "f</h5>");
+      
+        $(".collapsible").append(newConditions);
       }
     }
   })
