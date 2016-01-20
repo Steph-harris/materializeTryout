a9242381ec0b837d26ba64248203870cd091d7d2
steph-harris <steph.harris21@gmail.com>
2016-01-20 16:25:55 -0500

    added weather underground API and loop to grab weather info for next 3 days

--- scripts/test.js
+++ scripts/test.js
@@ -2,4 +2,20 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
 
   $('.parallax').parallax();
+
+  //for each item in collection, run ajax request to return city weather info
+  $.ajax({
+    type: "GET",
+    url: "http://api.wunderground.com/api/d736e7063cf1620b/conditions/forecast/q/08873.json",
+    dataType: "jsonp",
+    success: function(data){
+      var weather = data.forecast.simpleforecast.forecastday;
+
+      console.log(weather);
+
+      for(i=0; i<weather.length; i++){
+        console.log("Conditions for " + weather[i].date.pretty + ": " + weather[i].conditions + " with a high of " + weather[i].high.fahrenheit + "f, and a low of " + weather[i].low.fahrenheit + "f");
+      }
+    }
+  })
 });
