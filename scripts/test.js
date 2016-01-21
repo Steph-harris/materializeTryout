645498754bd2ce0a2f4fdf6f14f64f530432e412
steph-harris <steph.harris21@gmail.com>
2016-01-21 00:43:20 -0500

    added values for cities to be placed in weather api call, updated api call to return specific city weather

--- scripts/test.js
+++ scripts/test.js
@@ -4,20 +4,28 @@ $(document).ready(function(){
   $('.parallax').parallax();
 
   //for each list item in collapsible, run ajax request to return city weather info and append to that c-body
-  $.ajax({
-    type: "GET",
-    url: "http://api.wunderground.com/api/d736e7063cf1620b/conditions/forecast/q/08873.json",
-    dataType: "jsonp",
-    success: function(data){
-      var weather = data.forecast.simpleforecast.forecastday;
+  $(".collapsible-header").on("click",function(){
+    var cityInfo = $(this).attr("value");
+    alert(cityInfo);
+    $.ajax({
+      type: "GET",
+      url: "http://api.wunderground.com/api/d736e7063cf1620b/conditions/forecast/q/" + cityInfo + ".json",
+      dataType: "jsonp",
+      success: function(data){
+        var weather = data.forecast.simpleforecast.forecastday;
+        var city = data.current_observation.display_location.full;
 
-      console.log(weather);
+        console.log(city);
+        console.log(weather);
 
-      for(i=0; i<weather.length; i++){
-        var newConditions = ("<h5>Conditions for " + weather[i].date.pretty + ": " + weather[i].conditions + " with a high of " + weather[i].high.fahrenheit + "f, and a low of " + weather[i].low.fahrenheit + "f</h5>");
-      
-        $(".collapsible").append(newConditions);
+        $(".collapsible").append("<h5>Weather in " + city + "</h5>");
+
+        for(i=0; i<weather.length; i++){
+          var newConditions = ("<h5>Conditions for " + weather[i].date.pretty + ": " + weather[i].conditions + " with a high of " + weather[i].high.fahrenheit + "f, and a low of " + weather[i].low.fahrenheit + "f</h5>");
+        
+          $(".collapsible").append(newConditions);
+        }
       }
-    }
-  })
+    })
+  });
 });
